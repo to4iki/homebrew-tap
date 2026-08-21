0a971f7af82c0ee15d1581a072f74279717d0667
to4iki <to4iki@users.noreply.github.com>
2026-08-21 14:51:43 +0000

    Commit from GitHub Actions (update-formula)

--- Formula/rigmode.rb
+++ Formula/rigmode.rb
@@ -1,28 +1,28 @@
 class Rigmode < Formula
   desc "Attach work modes to AI coding agent prompts"
   homepage "https://github.com/to4iki/rigmode"
-  version "0.1.0"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/to4iki/rigmode/releases/download/v0.1.0/rigmode-darwin-arm64.tar.gz"
-      sha256 "5deb3451a8540e4cdc3732773f1d0d8306b8c7679f1cb83e221f275e475a27e4"
+      url "https://github.com/to4iki/rigmode/releases/download/v0.1.2/rigmode-darwin-arm64.tar.gz"
+      sha256 "9276e066515f7d864ede3f6f4a13b028cfff1c97c52d05dde0692a46988f4b5f"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/to4iki/rigmode/releases/download/v0.1.0/rigmode-darwin-amd64.tar.gz"
-      sha256 "012f75761e054bece09e307f9f882fa8eec16c1281366fc3b02810e0acd511a9"
+      url "https://github.com/to4iki/rigmode/releases/download/v0.1.2/rigmode-darwin-amd64.tar.gz"
+      sha256 "6ac8ece139c5f07393a0298c702418af0edabf5898b2b44889994f87de3b6e5b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/to4iki/rigmode/releases/download/v0.1.0/rigmode-linux-arm64.tar.gz"
-      sha256 "748a4e346309e58cb634415b3f37a34533e46ef39a52441d5e5b718f53113e53"
+      url "https://github.com/to4iki/rigmode/releases/download/v0.1.2/rigmode-linux-arm64.tar.gz"
+      sha256 "0cd16072ff488933225722373fd8ffdf1b8f6aa625a0bf15aeb53b49c0d632d5"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/to4iki/rigmode/releases/download/v0.1.0/rigmode-linux-amd64.tar.gz"
-      sha256 "6a7beda85bd30e27aafceff11ae82189eba590640ac4df0e72d13d3ac4d00984"
+      url "https://github.com/to4iki/rigmode/releases/download/v0.1.2/rigmode-linux-amd64.tar.gz"
+      sha256 "272032041c29883348cc5e2154e74a1391d4a036f1ff3db9008975fa78d0e9ed"
     end
   end
 
